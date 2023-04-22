83fb7a807f7708034ff9590f70f9a26101c9eb05
nagasakimachine <afroprice@hotmail.com>
2023-04-22 23:29:09 +0200

    Layouts & Actions

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,29 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 // Routes
 import Dashboard, { dashboardLoader } from './pages/Dashboard';
 import Error from './pages/Error';
+// Layouts
+import Main, { mainLoader } from './layouts/Main';
+// Actions
+import { logoutAction } from './actions/logout';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Dashboard/>,
-    loader: dashboardLoader,
-    errorElement: <Error/>
+    element: <Main/>,
+    loader: mainLoader,
+    errorElement: <Error/>,
+    children: [
+      {
+        index: true,
+        element: <Dashboard/>,
+        loader: dashboardLoader,
+        errorElement: <Error/>
+      },
+      {
+        path: 'logout',
+        action: logoutAction
+      }
+    ]
   },
   
 ]);

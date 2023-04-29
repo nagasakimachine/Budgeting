71f6cc884bac6f4a6dd0d7c13f5d1a1945161551
nagasakimachine <afroprice@hotmail.com>
2023-04-29 08:30:21 +0200

    New Budget Form

---
+++ src/components/AddBudgetForm.jsx
@@ -0,0 +1,45 @@
+// library imports
+import { CurrencyDollarIcon } from '@heroicons/react/24/solid'
+import { Form } from 'react-router-dom'
+
+const AddBudgetForm = () => {
+  return (
+    <div className='form-wrapper'>
+      <h2 className='h3'>
+        Create budget
+      </h2>
+      <Form
+        method='post'
+        className='grid-sm'
+      >
+        <div className='grid-xs'>
+            <label htmlFor='newBudget'>Budget Name</label>
+            <input 
+                type='text' 
+                name='newBudget' 
+                id='newBudget'
+                placeholder='e.g., Groceries'
+                required 
+            />
+        </div>
+        <div className='grid-xs'>
+            <label htmlFor='newBudgetAmount'>Amount</label>
+            <input 
+                type='number'
+                step='0.01'
+                name='newBudgetAmount'
+                placeholder='e.g., $350'
+                required
+                inputMode='decimal'
+            />
+        </div>
+        <button type='submit' className='btn--dark'>
+            <span>Create budget</span>
+            <CurrencyDollarIcon width={20} />
+        </button>
+      </Form>
+    </div>
+  )
+}
+
+export default AddBudgetForm

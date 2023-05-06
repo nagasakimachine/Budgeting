9a3bf84e89318c023f33f457a83ad10a6a9066f0
nagasakimachine <afroprice@hotmail.com>
2023-05-06 10:03:42 +0200

    Budget Cards

--- src/components/AddExpenseForm.jsx
+++ src/components/AddExpenseForm.jsx
@@ -6,7 +6,7 @@ import { useFetcher } from "react-router-dom"
 // library imports
 import { PlusCircleIcon } from "@heroicons/react/24/solid";
 
-const AddExpenseForm = ({budgets}) => {
+const AddExpenseForm = ({ budgets }) => {
     const fetcher = useFetcher();
     const isSubmitting = fetcher.state === 'submitting';
 
--- src/helpers.js
+++ src/helpers.js
@@ -41,3 +41,36 @@ export const createExpense = ({ name, amount, budgetId }) => {
 export const deleteItem = ({key}) => {
     return localStorage.removeItem(key)
 }
+
+// total spent by budget
+export const calculateSpentByBudget = (budgetId) => {
+    const expenses = fetchData('expenses') ?? [];
+    const budgetSpent = expenses.reduce((acc, expense) => {
+        // check if expense.id === budgetId I passed in
+        if(expense.budgetId !== budgetId)
+        return acc;
+        // add the current amount to my total
+        return acc += expense.amount;
+    }, 0)
+    return budgetSpent;
+}
+
+
+// FORMATTING
+
+// formatting percentage
+export const formatPercentage = (amt) => {
+    return amt.toLocaleString(undefined, {
+        style: 'percent',
+        minimumFractionDigits: 0,
+    })
+}
+
+
+// Format currency
+export const formatCurrency = (amt) => {
+    return amt.toLocaleString(undefined, {
+        style: 'currency',
+        currency: 'USD'
+    })
+}
